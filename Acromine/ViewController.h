802cd8d99063d817881a116a28b4e63e59a0e528
Swapnil Ratnaparkhi <swapnil_ratnaparkhi@choicehotels.com>
2016-03-09 20:53:04 -0700

    Initial Commit

---
+++ Acromine/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Acromine
+//
+//  Created by Swapnil Ratnaparkhi on 2016-03-09.
+//  Copyright © 2016 Swapnil Ratnaparkhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Acromine/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Acromine
+//
+//  Created by Swapnil Ratnaparkhi on 2016-03-09.
+//  Copyright © 2016 Swapnil Ratnaparkhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
